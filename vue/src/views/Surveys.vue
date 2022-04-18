31d366262084324f246e67d76b84ddb604715c98
MULTIBLITY\ilham <ilham@multiblity.com>
2022-04-18 15:57:27 +0530

    Pages setup

--- vue/src/views/Surveys.vue
+++ vue/src/views/Surveys.vue
@@ -2,7 +2,7 @@
     <span>
         <header class="bg-white shadow">
         <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-            <h1 class="text-3xl font-bold text-gray-900">Dashboard</h1>
+            <h1 class="text-3xl font-bold text-gray-900">Surveys</h1>
         </div>
         </header>
         <main>
@@ -19,6 +19,6 @@
 
 <script>
 export default {
-    
+  
 }
 </script>
